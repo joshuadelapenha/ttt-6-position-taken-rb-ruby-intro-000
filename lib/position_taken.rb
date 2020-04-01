19d693995e6d127c370e35a52837a918de0adbb0
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-01 15:30:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,1 @@
-# code your #position_taken? method here!
-def position_taken?
-
-  if board[index] == " " ||board[index] == "" board[index] == nil
-      taken
-  end
-
-end
+# code your #position_taken? method here
